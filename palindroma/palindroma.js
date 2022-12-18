cefc8d271ad59d609ac0f8ba8739c4943edc248b
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-18 17:19:20 +0100

    get doms element in js

---
+++ palindroma/palindroma.js
@@ -0,0 +1,9 @@
+/* 
+Chiedere all’utente di inserire una parola tramite un form.
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+//* DOM's Elements
+const formElement = document.querySelector('form');
+const inputElement = document.querySelector('input');
+const targetElement = document.getElementById('target');
