0e8d57071ff6c20b2f446c1e84fa3bfe94238f67
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-18 17:34:08 +0100

    function 'isPalindrome'

--- palindroma/palindroma.js
+++ palindroma/palindroma.js
@@ -3,6 +3,17 @@ Chiedere all’utente di inserire una parola tramite un form.
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
+//* FUNCTIONS - in this case, only one
+const isPalindrome = (inputValue) => {
+    
+    let inputValueReversed = '';
+    for (let i = inputValue.length - 1; i >= 0; i--){
+        inputValueReversed += inputValue[i]
+    }
+     return inputValue === inputValueReversed
+}
+
+
 //* DOM's Elements
 const formElement = document.querySelector('form');
 const inputElement = document.querySelector('input');
@@ -14,6 +25,9 @@ formElement.addEventListener('submit', function(e){
     e.preventDefault(); // nedeed not to send form's value to backend
 
     const inputValue = inputElement.value.trim();
-    console.log(inputValue);
+
+    //! validation: TO DO
+
+
 
 })
