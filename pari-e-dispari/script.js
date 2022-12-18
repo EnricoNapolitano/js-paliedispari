476c1607ef284c44889582c6156b21a8ce07ad21
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-18 18:06:54 +0100

    dom's elements

---
+++ pari-e-dispari/script.js
@@ -0,0 +1,12 @@
+/*
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5 tramite un form.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+//* DOMS elements
+const userNumberTypeElement = document.getElementById('user-number-type');
+const userNumberElement = document.getElementById('user-number');
+console.log(userNumberElement, userNumberTypeElement);
