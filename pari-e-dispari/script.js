e8dbf587ca4de3673db44dc8af0a25b9166f664f
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-18 18:42:34 +0100

    functions

--- pari-e-dispari/script.js
+++ pari-e-dispari/script.js
@@ -6,19 +6,34 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
 
-//* DOMS elements
+
+
+//* FUNCTIONS
+const sum = (randomNumber, userNumber) => randomNumber + userNumber;
+const typeOfNumber = (randomNumber, userNumber) => message = !(sum(randomNumber, userNumber) % 2) ? 'la somma è pari' : 'la somma è dispari';
+
+
+//* DOM'S ELEMENTS
 const userNumberTypeElement = document.getElementById('user-number-type');
 const userNumberElement = document.getElementById('user-number');
 const formElement = document.querySelector('form');
+const targetElement = document.getElementById('target');
+let message = '';
 
 
+//* SUBMIT EVENT
 formElement.addEventListener('submit', function(e){
     e.preventDefault();
-    
+
     const randomNumber = Math.floor(Math.random()*5)+1; // CPU number
+    console.log(randomNumber);
     const userNumber = parseInt(userNumberElement.value.trim());
     const userNumberType = userNumberTypeElement.value;
-    console.log(randomNumber, userNumber, userNumberType)
     
+    sum(randomNumber, userNumber) // sum function
+    console.log(sum(randomNumber, userNumber));
+    typeOfNumber(randomNumber, userNumber) // function that release if sum is an even or an odd number
+    console.log(typeOfNumber(randomNumber, userNumber));
 
+    targetElement.innerText = message;
 })
