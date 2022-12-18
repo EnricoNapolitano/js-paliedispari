e9efc25a6c2dcc34b8ae92010d980378e8865523
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-18 18:17:43 +0100

    get input value

--- pari-e-dispari/script.js
+++ pari-e-dispari/script.js
@@ -9,7 +9,16 @@ Dichiariamo chi ha vinto.
 //* DOMS elements
 const userNumberTypeElement = document.getElementById('user-number-type');
 const userNumberElement = document.getElementById('user-number');
-const buttonElement = document.querySelector('button');
+const formElement = document.querySelector('form');
 
-const randomNumber = Math.floor(Math.random()*5)+1;
-console.log(randomNumber)
+
+formElement.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    const randomNumber = Math.floor(Math.random()*5)+1; // CPU number
+    const userNumber = parseInt(userNumberElement.value.trim());
+    const userNumberType = userNumberTypeElement.value;
+    console.log(randomNumber, userNumber, userNumberType)
+    
+
+})
